9746876c910e9af02be1f8f480f3a8c8c5bc6e59
root <root@as2310offline.mirhpnm4l0rubm30pmmcyezdvb.ax.internal.cloudapp.net>
2025-11-04 07:08:25 +0000

    Adding mig mutating webhook sources

---
+++ migmutatingwebhook/pkg/configreader/configreader.go
@@ -0,0 +1,49 @@
+package configreader
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type ModernDU struct {
+	Enabled     bool        `json:"enabled"`
+	Migsettings MigSettings `json:"mig-settgins"`
+}
+
+type MigSettings struct {
+	Ocr       string `json:"ocr"`
+	Extractor string `json:"extractor"`
+	Training  string `json:"training"`
+	Oob       string `json:"oob"`
+	Resource  string `json:"resource"`
+}
+
+type ClassicDU struct {
+	Resource string `json:"resource"`
+}
+
+type Configuratie struct {
+	ModernDU  ModernDU  `json:"modern-du"`
+	ClassicDU ClassicDU `json:"classic-du"`
+}
+
+func Readconfig() Configuratie {
+
+	configData, err := os.ReadFile("/config/config.json")
+
+	if err != nil {
+
+		log.Fatalln("Unable to read config file. Does the configmap exist? ", err)
+	}
+
+	var jsonconfig Configuratie
+
+	jsonerr := json.Unmarshal(configData, &jsonconfig)
+
+	if jsonerr != nil {
+
+		log.Fatalln("Unable to unmarshal json configuration. ", jsonerr)
+	}
+	return jsonconfig
+}
